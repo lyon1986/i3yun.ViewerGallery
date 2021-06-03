636bd5737d0d1354225d286830d9b84c815642ca
崔鹤 <cuihe@siiri.cn>
2021-06-03 10:49:21 +0800

    sample2021

--- src/MinimumSample2021/index.js
+++ src/MinimumSample2021/index.js
@@ -5,17 +5,32 @@ Sippreep.Initializer().then(() => {
   let htmlDivElement = document.getElementById('SippreepViewer');
   viewer = new Sippreep.Viewing.GuiViewer3D(htmlDivElement);
   const errorCode = viewer.start();
-  if (errorCode > 0) {
-    console.error('Failed to create a Viewer: WebGL not supported.');
-    return;
-  }
-  console.log('Initialization complete, loading a model next...');
+  // if (errorCode > 0) {
+  //   console.error('Failed to create a Viewer: WebGL not supported.');
+  //   return;
+  // }
+  // console.log('Initialization complete, loading a model next...');
 
-  let url1 = `http://api.aisanwei.cn/api/Storge/Viewable?ID=`;
+  const url1 = `http://api.aisanwei.cn/api/Storge/Viewable?ID=`;
+
+  /**
+   * url2 这部分数据是从转换接口的返回中获取的
+   *
+   * ```ts
+   * let mainFileID = res.manifest.derivatives.find((v) => {
+   * return v.endsWith('.hf');
+   * });
+   * let url = `http://api.aisanwei.cn/api/Storge/Viewable?ID=` + mainFileID;
+   * ```
+   *
+   */
   let url2 = `jobs/ef819df5-ae58-4cb5-b558-61c49f75b6d8/output/main.hf`;
   viewer.loadModel(url1 + url2, {}, onSucceed, onFailed);
   function onSucceed(model) {
-    console.log(model);
+    console.log(`模型加载成功 ${model}`);
+    /**
+     * 您的业务内容一般写在这里（特殊情况如需要控制加载模型的相关，请与我们联系）
+     */
   }
   function onFailed(error) {
     console.warn(error);
