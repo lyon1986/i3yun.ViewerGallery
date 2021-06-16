4739be71abc257f266a9750a080902def0cae75e
崔鹤 <cuihe@siiri.cn>
2021-06-16 09:37:30 +0800

    update js

--- src/ViewerWithoutGui2021/pick-plane/index.js
+++ src/ViewerWithoutGui2021/pick-plane/index.js
@@ -18,12 +18,18 @@ Sippreep.Initializer().then(() => {
     pickPlane.then((ext) => {
       console.log(ext); // 这是插件
       ext.enableMode(Sippreep.Extensions.PickPlane.PickPlaneMode.NegativeDirectionOfX);
-      ext.registerPlaneCallback((p) => {
+      ext.registerPlaneCallback((p,v4) => {
+        /*
         // console.log(p);
         let planes = viewer.getCutPlanes();
         planes.push(new THREE.Vector4(p.normal.x, p.normal.y, p.normal.z, p.constant));
         viewer.setCutPlanes(planes);
         // viewer.setCutPlanes([]);
+        */
+        let planes = viewer.getCutPlanes();
+        planes.push(v4);
+        viewer.setCutPlanes(planes);
+
       });
     });
   }
