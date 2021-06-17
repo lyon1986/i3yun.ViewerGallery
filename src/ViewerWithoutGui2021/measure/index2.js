43976559c5359c77a1ddbb88b086f04357eb4edb
崔鹤 <cuihe@siiri.cn>
2021-06-17 10:18:07 +0800

    eslint fix

--- src/ViewerWithoutGui2021/measure/index2.js
+++ src/ViewerWithoutGui2021/measure/index2.js
@@ -6,8 +6,8 @@ Sippreep.Initializer().then(() => {
   viewer = new Sippreep.Viewing.Viewer3D(document.getElementById('SippreepViewer'));
   const viewerStartErrorCode = viewer.start();
 
-  const url1 = `http://api.aisanwei.cn/api/Storge/Viewable?ID=`;
-  let url2 = `jobs/ef819df5-ae58-4cb5-b558-61c49f75b6d8/output/main.hf`;
+  const url1 = 'http://api.aisanwei.cn/api/Storge/Viewable?ID=';
+  let url2 = 'jobs/ef819df5-ae58-4cb5-b558-61c49f75b6d8/output/main.hf';
   viewer.loadModel(url1 + url2, {}, onSucceed, onFailed); //第二个参数，如果在加载多模型时候模型位置错乱，则需要填写，正常时不用管
 
   function onSucceed(model) {
@@ -25,8 +25,8 @@ Sippreep.Initializer().then(() => {
     viewer = new Sippreep.Viewing.Viewer3D(document.getElementById('SippreepViewer'));
     const viewerStartErrorCode = viewer.start();
 
-    const url1 = `http://api.aisanwei.cn/api/Storge/Viewable?ID=`;
-    let url2 = `jobs/ef819df5-ae58-4cb5-b558-61c49f75b6d8/output/main.hf`;
+    const url1 = 'http://api.aisanwei.cn/api/Storge/Viewable?ID=';
+    let url2 = 'jobs/ef819df5-ae58-4cb5-b558-61c49f75b6d8/output/main.hf';
     viewer.loadModel(url1 + url2, {}, onSucceed2, onFailed);
   }
 
